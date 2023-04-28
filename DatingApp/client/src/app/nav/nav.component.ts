eb8e58b4b9b738a32f0864bc09b9b605de4e73fd
Shubham Prajapati <shubham.cosmotechai@gmail.com>
2023-04-28 10:29:18 +0530

    authentication completed

--- DatingApp/client/src/app/nav/nav.component.ts
+++ DatingApp/client/src/app/nav/nav.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
-import { Observable, of } from 'rxjs';
-import { User } from '../_models/user';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -12,7 +12,7 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService , private router: Router, private toastr: ToastrService) { }
 
   ngOnInit(): void {
     
@@ -20,15 +20,13 @@ export class NavComponent implements OnInit {
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log(response);
-      },
-      error: err => console.log(err)
+      next: () => this.router.navigateByUrl('/members'),
+      error: err => this.toastr.error(err.error)
     });
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
-
