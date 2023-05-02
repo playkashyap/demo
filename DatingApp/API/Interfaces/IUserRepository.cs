f329e4358b3ff41438d916fd99437a47478f5970
Shubham Prajapati <shubham.cosmotechai@gmail.com>
2023-05-02 17:34:38 +0530

    added data for working and optimiations

---
+++ DatingApp/API/Interfaces/IUserRepository.cs
@@ -0,0 +1,19 @@
+
+using API.DTOs;
+using API.Entities;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace API.Interfaces
+{
+    public interface IUserRepository
+    {
+        void Update (AppUser user);
+
+        Task<bool> SaveAllAsunc();
+        Task <IEnumerable<AppUser>> GetUsersAsync();
+        Task<AppUser> GetUserByIdAsync(int id);
+        Task<AppUser> GetUserByUsernameAsync(string username);
+        Task<IEnumerable<MemberDto>> GetMembersAsync();
+        Task<MemberDto> GetMemberAsync(string username);
+    }
+}
